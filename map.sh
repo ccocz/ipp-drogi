dd28cfc899370c654e4e29409997e30674a1dd1a
resul <rh402185@students.mimuw.edu.pl>
2019-05-18 16:06:34 +0200

    before last

--- map.sh
+++ map.sh
@@ -11,10 +11,21 @@ shift
 while [[ $# -ne 0 ]]
 do
     ARG="$1"
-    grep ^"${ARG};" ${FILE}
+    ANS=0
+    CURRENT=$(grep ^"${ARG};" ${FILE} | cut -d ';' -f 3-)
+    while [[ 1 == 1 ]]
+    do
+        MAL=$(echo ${CURRENT} | cut -d ';' -f 1)
+        ANS=$(echo "$((MAL + ANS))")
+        CURRENT=$(echo ${CURRENT} | cut -d ';' -f 4-)
+        if [[ -z ${CURRENT} ]]
+        then
+            break;
+        fi
+    done
+    if [[ ${ANS} != 0 ]]
+    then
+        echo ${ANS}
+    fi
     shift
 done
-
-#grep ^'13;'
-
-#sed
